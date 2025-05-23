47e7550cfb779bf8ab4efc5f5c9a05967757fb9d
Enzo <enhance674@gmail.com>
2025-05-23 15:09:59 +0200

    doc and fix sql request

--- src/main/java/org/example/dao/CustomerDAO.java
+++ src/main/java/org/example/dao/CustomerDAO.java
@@ -1,7 +1,6 @@
 package org.example.dao;
 
 import org.example.model.Customer;
-
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -9,84 +8,87 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CustomerDAO extends BaseDAO<Customer>{
-    //Constructor
+public class CustomerDAO extends BaseDAO<Customer> {
+    // Constructor
     public CustomerDAO(Connection connection) {
         super(connection);
     }
 
-    //Methods
+    // Methods
     /**
-     * Create a customer in database
-     * @param customer
-     * @return boolean
-     * @throws SQLException
+     * Create a new customer in the database.
+     *
+     * @param customer the Customer object to save in the database
+     * @return true if the customer was successfully saved, false otherwise
+     * @throws SQLException if a database access error occurs
      */
     @Override
-    public boolean create(Customer customer) throws SQLException{
-        request = "INSERT INTO customer (last_name,first_name,phone) values (?,?,?)";
+    public boolean create(Customer customer) throws SQLException {
+        request = "INSERT INTO customer (last_name, first_name, phone) VALUES (?, ?, ?)";
         preparedStatement = connection.prepareStatement(request, Statement.RETURN_GENERATED_KEYS);
         preparedStatement.setString(1, customer.getLastName());
         preparedStatement.setString(2, customer.getFirstName());
         preparedStatement.setString(3, customer.getPhone());
         int nbRow = preparedStatement.executeUpdate();
         resultSet = preparedStatement.getGeneratedKeys();
-        if (resultSet.next()){
+        if (resultSet.next()) {
             customer.setIdCustomer(resultSet.getInt(1));
         }
         return nbRow == 1;
     }
 
     /**
-     * Update a customer datas within database
-     * @param customer
-     * @return boolean
-     * @throws SQLException
+     * Update an existing customer's data in the database.
+     *
+     * @param customer the Customer object containing updated information
+     * @return true if the customer was successfully updated, false otherwise
+     * @throws SQLException if a database access error occurs
      */
     @Override
-    public boolean update(Customer customer) throws SQLException{
-        request = "UPDATE Customer SET last_name = ?, first_name = ?, phone = ?";
+    public boolean update(Customer customer) throws SQLException {
+        request = "UPDATE customer SET last_name = ?, first_name = ?, phone = ? WHERE id_customer = ?";
         preparedStatement = connection.prepareStatement(request);
         preparedStatement.setString(1, customer.getLastName());
         preparedStatement.setString(2, customer.getFirstName());
         preparedStatement.setString(3, customer.getPhone());
+        preparedStatement.setInt(4, customer.getIdCustomer());
         int nbRow = preparedStatement.executeUpdate();
         return nbRow == 1;
     }
 
     /**
-     * Delete a customer from database
-     * @param customer
-     * @return boolean
-     * @throws SQLException
+     * Delete a customer from the database.
+     *
+     * @param customer the Customer object to delete
+     * @return true if the customer was successfully deleted, false otherwise
+     * @throws SQLException if a database access error occurs
      */
     @Override
-    public boolean delete(Customer customer) throws SQLException{
-        request = "UPDATE Customer SET last_name = ?, first_name = ?, phone = ?";
+    public boolean delete(Customer customer) throws SQLException {
+        request = "DELETE FROM customer WHERE id_customer = ?";
         preparedStatement = connection.prepareStatement(request);
-        preparedStatement.setString(1, customer.getLastName());
-        preparedStatement.setString(2, customer.getFirstName());
-        preparedStatement.setString(3, customer.getPhone());
+        preparedStatement.setInt(1, customer.getIdCustomer());
         int nbRow = preparedStatement.executeUpdate();
         return nbRow == 1;
     }
 
     /**
-     * Get a customer from database
-     * @param id
-     * @return Customer
-     * @throws SQLException
+     * Retrieve a customer from the database by their ID.
+     *
+     * @param id the ID of the customer to retrieve
+     * @return the Customer object if found, or null if not found
+     * @throws SQLException if a database access error occurs
      */
     @Override
-    public Customer get(int id) throws SQLException{
+    public Customer get(int id) throws SQLException {
         Customer customer = null;
-        request = "SELECT * FROM Customer WHERE id_customer = ?";
+        request = "SELECT * FROM customer WHERE id_customer = ?";
         preparedStatement = connection.prepareStatement(request);
         preparedStatement.setInt(1, id);
         resultSet = preparedStatement.executeQuery();
-        if(resultSet.next()){
+        if (resultSet.next()) {
             customer = new Customer(
-                    resultSet.getInt("idCustomer"),
+                    resultSet.getInt("id_customer"),
                     resultSet.getString("last_name"),
                     resultSet.getString("first_name"),
                     resultSet.getString("phone")
@@ -96,17 +98,18 @@ public class CustomerDAO extends BaseDAO<Customer>{
     }
 
     /**
-     * Get all customers from database
-     * @return List(Customer)
-     * @throws SQLException
+     * Retrieve all customers from the database.
+     *
+     * @return a List of all Customer objects in the database
+     * @throws SQLException if a database access error occurs
      */
     @Override
-    public List<Customer> getAll() throws SQLException{
+    public List<Customer> getAll() throws SQLException {
         List<Customer> customers = new ArrayList<>();
-        request = "SELECT * FROM Customer";
+        request = "SELECT * FROM customer";
         preparedStatement = connection.prepareStatement(request);
         resultSet = preparedStatement.executeQuery();
-        while (resultSet.next()){
+        while (resultSet.next()) {
             Customer customer = new Customer(
                     resultSet.getInt("id_customer"),
                     resultSet.getString("last_name"),
