3b40685d8492a103f364ca4ba431ee176a0a292d
Enzo <enhance674@gmail.com>
2025-05-23 14:04:55 +0200

    Customer class

--- src/main/java/org/example/model/Customer.java
+++ src/main/java/org/example/model/Customer.java
@@ -1,4 +1,66 @@
 package org.example.model;
 
-public class Customer {
+public class Customer{
+    //Properties
+    private int idCustomer;
+    private String firstName;
+    private String lastName;
+    private String phone;
+
+    //Constructors
+    public Customer(int idCustomer, String firstName, String lastName, String phone) {
+        this.idCustomer = idCustomer;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+    }
+
+    public Customer(String firstName, String lastName, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+    }
+
+    //Methods
+    public int getIdCustomer(){
+        return idCustomer;
+    }
+
+    public void setIdCustomer(int idCustomer){
+        this.idCustomer = idCustomer;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getPhone(){
+        return phone;
+    }
+
+    public void setPhone(String phone){
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString(){
+        return "Customer{" +
+                "idCustomer=" + idCustomer +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
 }
